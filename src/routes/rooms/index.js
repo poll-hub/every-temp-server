08abc7d6fba91f81e54cd05279109f24f745f221
gonudayo <kgw010119@gmail.com>
2025-05-06 23:02:01 +0900

    feat: room list

--- src/routes/rooms/index.js
+++ src/routes/rooms/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const ctrl = require("./rooms.ctrl");
 
+router.get("/", ctrl.get.list); // 방 전체목록
 router.post("/create", ctrl.post.create); // 방 만들기
 
 module.exports = router;
--- src/routes/rooms/rooms.ctrl.js
+++ src/routes/rooms/rooms.ctrl.js
@@ -2,10 +2,29 @@ const { User } = require("../../models/User/User");
 const { Room } = require("../../models/Room/Room");
 const { OAuth2Client } = require("google-auth-library");
 
-const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+const get = {
+  list: async (req, res) => {
+    try {
+      const rooms = await Room.find({}).lean();
+
+      return res.status(200).json({
+        success: true,
+        rooms,
+      });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  },
+};
 
 const post = {
   create: async (req, res) => {
+    const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
     const { idToken } = req.cookies;
     const { name, description } = req.body;
 
@@ -50,5 +69,6 @@ const post = {
 };
 
 module.exports = {
+  get,
   post,
 };
